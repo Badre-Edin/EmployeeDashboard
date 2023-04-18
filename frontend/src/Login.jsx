0d7a73f33b48faa4ad00f278840393a727cc9341
Badre-Edin <dridibadr@gmail.com>
2023-04-18 23:58:59 +0100

    connection to database

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,25 +1,47 @@
 import React from 'react'
-
+import './style.css'
+import axios from 'axios'
+ 
 function Login() {
+
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('', values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
+
   return (
-    <div className='d-flex justify-content-center align-items-center vh-100 '>
-            <div className='bg-white p-3 rounded w-25'>
+    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+
+          <div className='nav'>
+            <p>
+                TMI
+            </p>
+          </div>
         
-                <h2>Login</h2>
-                <form>
+            <div className='p-3 rounded w-25 border loginForm'>
+        
+                <h2 className='text-center'>Login</h2>
+                <form onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
                         <input type="email" placeholder='Enter Email' name='email' 
-                          className='form-control rounded-0'/>
+                          onChange={e => setValues({...values, email: e.target.value})} className='form-control rounded-0'/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
                         <input type="password" placeholder='Enter Password' name='password'
-                          className='form-control rounded-0' />
+                          onChange={e => setValues({...values, password: e.target.value})} className='form-control rounded-0' />
                     </div>
                     <button type='submit' className='btn btn-success w-100 rounded-0'> Log in</button>
                     <p>You are agree to our terms and policies</p>
-                    <button className='btn btn-default border w-100 bg-light rounded-0 text-decoration'>Create Account</button>
+                    <button className='btn btn-secondary border w-100 rounded-0'>Create Account</button>
                 </form>
             </div>
         </div>
